f7121f3475c5226b964fa86636b2a9d882952fab
Konstantinos Benidis <benidisk@gmail.com>
2017-11-29 16:28:33 +0800

    test and plots

--- R_buildignore/test-spEigen.R
+++ R_buildignore/test-spEigen.R
@@ -1,60 +1,59 @@
 test_that("NA check", {
-  X_na <- heavy_data$X
-  X_na[10, 10] <- NA
-  expect_error(momentsStudentt(X_na))
-  expect_error(momentsCauchy(X_na))
-  expect_error(momentsTyler(X_na))
+  m = 10
+  n = 15
+  q = 3
+
+  X <- matrix(rnorm(m*n), ncol=m)
+  X_na <- X
+  X_na[1, 1] <- NA
+  expect_error(spEigen(X_na, q, 0.5, data=TRUE))
+  expect_error(spEigen(cov(X_na), q, 0.5))
+  expect_error(spEigenCov(cov(X_na), q, 0.5))
+  expect_error(spEigen(X, NA, 0.5, data=TRUE))
+  expect_error(spEigen(cov(X), NA, 0.5))
+  expect_error(spEigenCov(cov(X), NA, 0.5))
+  expect_error(spEigen(X, q, NA, data=TRUE))
+  expect_error(spEigen(cov(X), q, NA))
+  expect_error(spEigenCov(cov(X), q, NA))
 })
 
+test_that("positive integer q check", {
+  expect_error(spEigen(X, -1, 0.5, data=TRUE))
+  expect_error(spEigen(X, 0, 0.5, data=TRUE))
+  expect_error(spEigen(X, 0.5, 0.5, data=TRUE))
+  expect_error(spEigen(cov(X), -1, 0.5))
+  expect_error(spEigen(cov(X), 0, 0.5))
+  expect_error(spEigen(cov(X), 0.5, 0.5))
+  expect_error(spEigenCov(cov(X), -1, 0.5))
+  expect_error(spEigenCov(cov(X), 0, 0.5))
+  expect_error(spEigenCov(cov(X), 0.5, 0.5))
+})
 
 test_that("input dimension check", {
-  expect_error(momentsStudentt(heavy_data$X[1, ]))  # T > 1?
-  expect_error(momentsStudentt(heavy_data$X[, 1]))  # N > 1?
+  expect_error(spEigen(X[, 1], q, 0.5, data=TRUE)) # m > 1
+  expect_error(spEigen(cov(X[, 1]), q, 0.5)) # m > 1
+  expect_error(spEigenCov(cov(X[, 1]), q, 0.5)) # m > 1
 
-  expect_error(momentsCauchy(heavy_data$X[1, ]))  # T > 1?
-  expect_error(momentsCauchy(heavy_data$X[, 1]))  # N > 1?
-
-  expect_error(momentsTyler(heavy_data$X[1, ]))  # T > 1?
-  expect_error(momentsTyler(heavy_data$X[, 1]))  # N > 1?
+  expect_error(spEigen(X[1, ], q, 0.5, data=TRUE)) # n > 1 (full rank)
+  expect_error(spEigen(cov(X[1, ]), q, 0.5)) # n > 1 (full rank)
+  expect_error(spEigenCov(cov(X[1, ]), q, 0.5)) # n > 1 (full rank)
 })
 
 
 test_that("output dimension check", {
-  res <- momentsStudentt(heavy_data$X)
-  expect_equal(length(res$mu), heavy_data$N)
-  expect_equal(nrow(res$cov), heavy_data$N)
-  expect_equal(ncol(res$cov), heavy_data$N)
-  expect_equal(length(res$nv), 1)
+  res <- spEigen(X, q, 0.5, data=TRUE)
+  expect_equal(length(res$vectors), m*q)
+  expect_equal(length(res$values), q)
+  expect_equal(length(res$standard_vectors), m*q)
 
-  res <- momentsCauchy(heavy_data$X)
-  expect_equal(length(res$mu), heavy_data$N)
-  expect_equal(nrow(res$cov), heavy_data$N)
-  expect_equal(ncol(res$cov), heavy_data$N)
+  res <- spEigen(cov(X), q, 0.5)
+  expect_equal(length(res$vectors), m*q)
+  expect_equal(length(res$values), q)
+  expect_equal(length(res$standard_vectors), m*q)
 
-  res <- momentsTyler(heavy_data$X)
-  expect_equal(length(res$mu), heavy_data$N)
-  expect_equal(nrow(res$cov), heavy_data$N)
-  expect_equal(ncol(res$cov), heavy_data$N)
+  res <- spEigenCov(cov(X), q, 0.5)
+  expect_equal(length(res$vectors), m*m)
+  expect_equal(length(res$values), m)
+  expect_equal(length(res$cov), m*m)
 })
 
-
-test_that("mu and cov estimation check", {
-  res <- momentsStudentt(heavy_data$X)
-  expect_lt(norm(res$mu - heavy_data$mu, "2"), 0.2390831 + 0.001)
-  expect_lt(norm(res$cov - heavy_data$cov, "F"), 5.651713 + 0.001)
-
-  res <- momentsCauchy(heavy_data$X)
-  expect_lt(norm(res$mu - heavy_data$mu, "2"), 0.2377818 + 0.001)
-  expect_lt(norm(res$cov - heavy_data$cov, "F"), 6.574427 + 0.001)
-
-  res <- momentsTyler(heavy_data$X)
-  expect_lt(norm(res$mu - heavy_data$mu, "2"), 0.7242409 + 0.001)
-  expect_lt(norm(res$cov - heavy_data$cov, "F"), 5.826894 + 0.001)
-})
-
-
-test_that("Gaussian mu and cov estimation check", {
-  res <- momentsStudentt(heavy_data$X, nv = 1e15)
-  expect_lt(norm(res$mu - colMeans(heavy_data$X), "2"), 1e-12)
-  expect_lt(norm(res$cov - ((heavy_data$T-1)/heavy_data$T)*cov(heavy_data$X), "F"), 1e-11)
-})
