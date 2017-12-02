a370fae7a1ae256eee6cb2185703852c09e5a245
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2017-12-02 17:04:39 +0800

    Fixing check() warnings...

--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -1,10 +1,16 @@
 ##
 ## User installation
 ##
+# Local installation
 install.packages(file.choose(), repos = NULL, type="source")
+# Installation from GitHub
+devtools::install_github("dppalomar/sparseEigen")
+# Installation from CRAN
+install.packages("sparseEigen")
+# Getting help
 library(sparseEigen)
 help(package="sparseEigen")
-?momentsStudentt
+?spEigen
 
 
 
@@ -21,10 +27,14 @@ devtools::install()
 devtools::build()  # to generate the installation file
 #devtools::use_readme_rmd()  # to create the README file
 #devtools::use_data_raw()  # to set up the raw-data folder
-#devtools::use_vignette("sparse_eigenvectors")
-devtools::build_vignettes() #or just install()
+
+#for vignettes
+#devtools::use_vignette("sparse_eigenvectors")  # to create the folder the first time
 #rmarkdown::render("vignettes/sparse_eigenvectors.Rmd", "md_document")  # this is to generate the .md for GitHub
-rmarkdown::render("vignettes/sparse_eigenvectors.Rmd", "all")
+rmarkdown::render("vignettes/sparse_eigenvectors.Rmd", "all")  # this also generates the pdf
+tools::compactPDF("vignettes/sparse_eigenvectors.pdf", gs_quality = "printer")  # this compresses the pdf
+devtools::build_vignettes() #or just install()
+
 
 # code checking
 lintr::lint_package()
@@ -42,9 +52,7 @@ covr::package_coverage()  #coverage of tests
 # CRAN check
 rcmdcheck::rcmdcheck()
 #R CMD check --as-cran  # this is before submission to CRAN
+#R CMD check --as-cran --compact-vignettes
 
-
-
-# to upload to CRAN or GitHub
-#devtools::install_github("yourusername/myfirstpackage")  # for GitHub
+# to upload to CRAN
 #devtools::release()  #for CRAN
